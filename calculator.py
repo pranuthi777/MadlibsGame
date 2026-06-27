e80b94698d2fd74bcba9e63c38e2bd1077a83fbd
pranuthi777 <pranuthigarapati2009@gmail.com>
2026-06-27 12:55:08 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#Madlibs Game
+adjective1 = input("pick an adjective (anything that gives a description: ")
+noun1 = input("pick a noun(name, place, animal, thing: ")
+verb1 = input("pick a verb(an action word )")
+
+print("i went to the zoo today")
+print("I saw a zebra which is + adjective1")
